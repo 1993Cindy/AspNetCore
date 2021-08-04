20f34c68cca5a429ee04fc9f20639cdaf2f85310
Cindy <2242580951@qq.com>
2021-08-04 23:44:49 +0800

    aspnet core web 实例

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DNetCoreTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
